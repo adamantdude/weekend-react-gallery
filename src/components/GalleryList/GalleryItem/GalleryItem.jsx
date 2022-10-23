186310710b9c7bb3fe7f03ff9e9907d637443101
adamantdude <adam.plee23@gmail.com>
2022-10-22 19:57:49 -0500

    remove repeating code

--- src/components/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem/GalleryItem.jsx
@@ -19,16 +19,14 @@ function GalleryItem({item, postFN, delFN}) {
 
     return (
         <div className="oneItem">
-            { viewMode &&
                 <div onClick={viewHandler} className="clickView">
-                    <p>{item.description}</p>
+                    { viewMode &&
+                        <p>{item.description}</p>
+                    }
+                    { !viewMode &&
+                        <img src={item.path} />
+                    }
                 </div>
-            }
-            { !viewMode &&
-                <div onClick={viewHandler} className="clickView">
-                    <img src={item.path} />
-                </div>
-            }
             <div>
                 <p>{item.likes} people love this!</p>
                 <button onClick={likeHandler}>Like!</button>
